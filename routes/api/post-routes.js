ec7ffb64af19df3839555fd9656126a1d1666621
Coco Puff <fahrija_wyroski@live.com>
2021-05-24 11:00:44 -0500

    added Vote

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
 //get all users
 router.get('/', (req, res) => {
+    console.log('====================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+            [sequelize.literal('SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -27,7 +31,9 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+            [sequelize.literal('SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -60,7 +66,17 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
-
+//PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    //custom static method creeated in models/Post.js
+    Post.upvote(req.body, { Vote })
+        //then find the post we just voted on
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
 router.put('/:id', (req, res) => {
     Post.update(
         {
@@ -73,8 +89,8 @@ router.put('/:id', (req, res) => {
         }
     )
         .then(dbPostData => {
-            if (!dbPostData[0]) {
-                res.status(404).json({ message: 'No post found with this id' });
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id!' });
                 return;
             }
             res.json(dbPostData);
@@ -84,7 +100,6 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
-
 router.delete('/:id', (req, res) => {
     Post.destroy({
         where: {
